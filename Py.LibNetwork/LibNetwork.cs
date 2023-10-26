d854377a45191e8225f62081ae0e1324aa46d5aa
Pyrrha <pyrrha.social@gmail.com>
2023-10-26 20:08:39 +1300

    feat: initial commit

---
+++ Py.LibNetwork/LibNetwork.cs
@@ -0,0 +1,40 @@
+﻿using Boardgame.Modding;
+using Boardgame.Networking;
+using Photon.Pun;
+using UnityEngine;
+using Utils;
+
+namespace Py.LibNetwork
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class LibNetwork : DemeoMod
+    {
+        public override void Load()
+        {
+            var gameObject = new GameObject("Py.LibNetwork");
+            gameObject.AddComponent<PhotonView>();
+            gameObject.AddComponent<ModNetwork>();
+
+            gameContext.networkController.JoinedRoom += ModNetwork.Instance.OnSelfJoinedRoom;
+            gameContext.networkController.LocalPlayerLeftRoom += ModNetwork.Instance.OnSelfLeftRoom;
+            gameContext.networkController.OtherPlayerJoined += ModNetwork.Instance.OnPlayerJoinedRoom;
+            gameContext.networkController.OtherPlayerDisconnected += ModNetwork.Instance.OnPlayerLeftRoom;
+            
+        }
+
+        public override ModdingAPI.ModInformation ModInformation { get; } = new()
+        {
+            name = "Py.LibNetwork",
+            version = "1.0.0",
+            author = "JustPyrrha",
+            description = "Networking utilities for mod developers.",
+            isNetworkCompatible = true
+        };
+
+        private const string LogPrefix = "Py.LibNetwork";
+
+        internal static void Log(string message) => DemeoLog.Log(LogPrefix, message);
+        internal static void LogWarn(string message) => DemeoLog.LogWarn(LogPrefix, message);
+        internal static void LogError(string message) => DemeoLog.LogError(LogPrefix, message);
+    }
+}
---
+++ Py.LibNetwork/ModNetwork.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Boardgame;
+using Boardgame.Modding;
+using Boardgame.Networking;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Py.LibNetwork
+{
+    public class ModNetwork : MonoBehaviour
+    {
+        public static ModNetwork Instance;
+        private PhotonView _photonView;
+        private GameContext _gameContext;
+        private readonly Dictionary<int, Dictionary<string, string>> _networkModLists = new();
+
+        #region RPC Methods
+        [PunRPC]
+        public void OnModListRPC(Dictionary<string, string> modList, PhotonMessageInfo info)
+        {
+            LibNetwork.Log($"Received ModList from {info.Sender.UserId}");
+            foreach (var (modName, version) in modList)
+            {
+                LibNetwork.Log($"{modName} ({version})");
+            }
+
+            _networkModLists[info.Sender.ActorNumber] = modList;
+        }
+
+        [PunRPC]
+        public void OnModEventIdSync(PhotonMessageInfo info)
+        {
+            // byte[]
+        }
+        #endregion
+
+        #region Room Events
+        public void OnSelfJoinedRoom()
+        {
+            BroadcastModList();
+        }
+
+        public void OnSelfLeftRoom()
+        {
+            _networkModLists.Clear();
+        }
+
+        public void OnPlayerJoinedRoom(NetworkPlayer player)
+        {
+            SendModList(player);
+            if(_photonView.AmOwner)
+                SyncModEventIds();
+        }
+
+        public void OnPlayerLeftRoom(NetworkPlayer player)
+        {
+            _networkModLists[player.ID.Value] = null;
+        }
+
+        #endregion
+
+        #region Network Calls
+        private void BroadcastModList()
+        {
+            if (!PhotonNetwork.IsConnected) return; // safety measure
+            _photonView.RPC(
+                nameof(OnModListRPC),
+                RpcTarget.Others,
+                ModdingAPI.GetInstalledMods().ToDictionary(mod => mod.name, mod => mod.version)
+            );
+        }
+        
+        private void SendModList(NetworkPlayer player)
+        {
+            if (!PhotonNetwork.IsConnected) return; // safety measure
+            _photonView.RPC(
+                nameof(OnModListRPC), 
+                PhotonNetwork.CurrentRoom.GetPlayer(player.ID.Value),
+                ModdingAPI.GetInstalledMods().ToDictionary(mod => mod.name, mod => mod.version)
+            );
+        }
+
+        private void SyncModEventIds()
+        {
+            _photonView.RPC(
+                nameof(OnModEventIdSync),
+                RpcTarget.All,
+                "test"
+            );
+        }
+        #endregion
+
+        public void SetGameContext(GameContext context)
+        {
+            _gameContext = context;
+        }
+        
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            
+            // Setup Photon View
+            _photonView = PhotonView.Get(this);
+            _photonView.ViewID = 900; // 0-999, we'll use a high view id to avoid conflicts with vanilla.
+            _photonView.OwnershipTransfer = OwnershipOption.Request;
+            _photonView.Synchronization = ViewSynchronization.UnreliableOnChange;
+            _photonView.observableSearch = PhotonView.ObservableSearch.AutoFindAll;
+
+            Instance = this;
+        }
+    }
+}
